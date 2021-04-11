c95966ca72497d7bfec96d6cd72e02772471aa75
Ala Obeidat <ala-work@hotmail.com>
2021-04-11 12:58:05 +0300

    change style of elements

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-
-
-
-
-const intervalToCheckInSeconds = 3;
+const intervalToCheckInSeconds = 5;
 const speedDisplay = document.getElementById("speed");
 const statusDisplay = document.getElementById("status");
 
@@ -11,21 +7,20 @@ const checkSlowConnection =  (seconds) => {
 	let speed="Excellent";
 	if(seconds > 1 && seconds < 3)
 		speed= "Good";
-	if(seconds > 3 && seconds < 5)
+	if(seconds > 3 && seconds < 10)
 		speed= "Poor";
-	if(seconds > 5 && seconds < 10)
-		speed= "Slow";
 	if(seconds > 10)
-		speed= "Very Slow"; 
+		speed= "Slow"; 
 	if(seconds < 0)
 		speed= "No"; 
 	speedDisplay.textContent = speed + ' Connection';
+    speedDisplay.className = 'speed-'+speed;
 }
 
 const checkOnlineStatus = async () => {
   try {
   let countDownDate = new Date().getTime();
-    const online = await fetch("http://1x1px.me/FF4D00-0.8.png",{mode: "no-cors"});
+    const online = await fetch("https://i.ibb.co/BBJv1Ff/FF4-D00-0-8-1.png",{mode: "no-cors"});
 	let newDate = new Date().getTime();
 	 let distance = newDate - countDownDate;
 	 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
@@ -35,13 +30,16 @@ const checkOnlineStatus = async () => {
   checkSlowConnection(-1);
     return false; // definitely offline
   }
+  finally{
+	  if(!_callEnded)
+		setTimeout(SetStatusDisplayContet,intervalToCheckInSeconds * 1000);
+  }
 };
 const SetStatusDisplayContet = async () => {
-statusDisplay.textContent = await checkOnlineStatus() ? "Online" : "OFFline";
+let result= await checkOnlineStatus() ? "Online" : "OFFline";
+statusDisplay.textContent = result;
+statusDisplay.className = 'result-'+result;
 }
-setInterval(async () => { 
-  await SetStatusDisplayContet();
-}, intervalToCheckInSeconds * 1000); 
 
 // forgot to include async load event listener in the video! 
 window.addEventListener("load", async (event) => { 
